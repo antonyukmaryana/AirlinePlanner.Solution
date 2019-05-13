1ba9c5eeeda176eb51b240c36e2e77312df5a779
maryanaantonyuk <amaryana@gmail.com>
2019-05-13 10:28:00 -0700

    add initial set up, models(city)

---
+++ AirlinePlanner/Models/City.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace AirlinePlanner
+{
+  public class City
+  {
+    private string _name;
+    private int _id;
+
+    public City(string name, int id = o)
+    {
+      _id = id;
+      _name = name;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SetId(int newId)
+    {
+      _id = newId;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName
+    }
+
+    public static List<City> GetAll()
+    {
+      List<City>allCities = new List<City>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CoomandText = @"SELECT * FROM City;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+
+      while (rdr.Read())
+      {
+        int cityId = rdr.GetInt32(0);
+        string cityName = rdr.GetString(1);
+
+        City city = new City(cityName, cityId);
+        allStylists.Add(city);
+      }
+
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+      return allCities;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO City (name) VALUES(@CityName);";
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@CityName";
+      name.Value = this._name;
+      cmd.Parameters.Add(name);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+
+    }
+  }
+}
---
+++ AirlinePlanner/Models/Flights.cs
@@ -0,0 +1,5 @@
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace AirlinePlanner.Models
